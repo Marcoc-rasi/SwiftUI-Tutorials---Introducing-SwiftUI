8b6fff667f3518e6d0b0e0b53d6a93392eceefbf
Marcos Uriel Martinez Ortiz <marcocu@comunidad.unam.mx>
2024-02-07 22:47:03 -0600

    update readme

---
+++ Chapter1-SwiftUI Essentials-marcocrasi/2-Building-List and-Navigation-marcocrasi/Landmarks-marcocrasi/Landmarks_marcocrasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_marcocrasiApp.swift
+//  Landmarks-marcocrasi
+//
+//  Created by Marcos Uriel Martinez Ortiz on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_marcocrasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chapter1-SwiftUI Essentials-marcocrasi/2-Building-List and-Navigation-marcocrasi/Landmarks-marcocrasi/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks-marcocrasi
+//
+//  Created by Marcos Uriel Martinez Ortiz on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
